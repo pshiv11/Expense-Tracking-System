be4c1bb5b70110cbe338febf138c408ee09cfa1b
pshiv11 <shivang.patel1@outlook.com>
2021-07-15 15:51:20 -0400

    added UI and Expenses folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
-import Expenses from './components/Expenses';
+import ExpenseItem from './components/Expenses/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 
 
 function App() {
